819b1c3f99097f21d207a795d935d12c9ab5eb86
Osuolale Emmanuel <59930570+dmarinere@users.noreply.github.com>
2021-01-29 07:40:47 +0100

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd 
+import datetime
 
 
 
@@ -15,22 +16,19 @@ def main():
     st.subheader("This Dashboard would help you understand how our fraud detection model works and what component would be needed")
         
     new = st.sidebar.selectbox("Would you want details on the model or see it in Action", ["Model Description", "Model in Action"])
-    if new == "Model Description": 
-        st.subheader('The inputs for this Model is the Account Number, Age, Sex, Marital Status, Transaction Amount, Date, Bank Account Status')
-        st.text("    ")
-        st.text("Please enter the account number, our model uses this to uniquely identify users")
-        account =st.number_input("Account number details please must be 10 digit", min_value=1000000000, max_value=9999999999)
-
-        st.text("Please enter your age, our model uses this value to predict ")
-        st.text("how likely you are to engage in this")
+    if new == "Model in Action":
+        st.markdown("Please enter your age, our model uses this value to predict \nhow likely you are to engage in this")
         age = st.number_input("Age range is from 10 to 90", min_value=10, max_value=90)
-        st.text("Please enter your gender")
+        st.markdown("Please enter your gender")
         gender =st.selectbox("What is your gender?", ["Male", "Female"])
-        st.text("Please select your marital Status")
+        st.markdown("Please select your marital Status")
         marital = st.selectbox("What is your marital Status", ["Single", "Divorced", "Married"])
-        st.text("Please input transaction amount, The model use this value and previous values")
-        st.text("already in the model for the account")
-        amount = st.number_input("What is the credit alert amount")
+        st.markdown("Please input transaction amount, The model use this value and previous value \nalready in the database for the account")
+        amount = st.number_input("What is the transaction amount")
+        st.markdown("Please input the day and time of this transaction")
+        st.date_input("What day did this transaction occur")
+        st.time_input("What time did this transaction occur")
+
 
         
 
